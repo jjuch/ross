bd2a2a6aa4a234da0cd1e28a7e26d4c53531f080
jjuchem <Jasper.Juchem@UGent.be>
2025-11-21 11:31:23 +0100

    deprecation issue numpy

--- ross/screw_element.py
+++ ross/screw_element.py
@@ -276,7 +276,7 @@ class ScrewElement(ShaftElement6DoF):
     def vertices_polar(cls, cs:Polygon):
         x, y = cls.vertices_cartesian(cs)
         r = np.sqrt(x**2 + y**2)
-        theta = np.atan2(x, y)
+        theta = np.arctan2(y, x)
         return r, theta
     
      
